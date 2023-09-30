e144d5e3d60cfa510fa2bf6581fb2fa5c3cc6887
jark <kark@kk.com>
2023-10-01 03:21:51 +0700

    。。。

--- echo.js
+++ echo.js
@@ -1,67 +1,37 @@
 //-------------start win
+var {readFileSync, writeFileSync, appendFileSync} = require("fs");
 
 
-function createWindow() {
-    // 创建浏览器窗口
-    const win = new BrowserWindow({
-        icon:"res/icon.jpg",
-        width: 1024,
-        height: 230,
-        webPreferences: {
-            nodeIntegration: true,
-            contextIsolation: false,
-            enableRemoteModule: true
-        }
-    })
-
- 
-
-    // 并且为你的应用加载index.html
-    win.loadFile('echo.html')
-
-    var ini= require("ini")
-    var path=require("path")
-    var fs=require("fs")
-    const iopath = path.join(__dirname, './cfg.ini'); // 引用Pos.ini的相对地址
-    const Info = ini.parse(fs.readFileSync(iopath, 'utf-8'));
-
-    require("./libx/logger")
-    log_info("Info.debug "+ Info.debug)
-    console.log("Info.debug "+ Info.debug)
-
-
-    // const fs = require('fs');
-
-// directory to check if exists
-    const dir = 'c:/debug';
-
-// check if directory exists
-
-
-        win.openDevTools();// win.openDevTools();
-
-
-
-}
-
-
-
+global['win'] = ""
+console.log(555)
 
 const {
     app, BrowserWindow,
     Menu,
     Tray
-} = require('electron')
+} = require('electron' +
+    '')
 const path = require("path");
 const ini = require("ini");
 const fs = require("fs");
+const electron = require("electron");
 
 
+
+function  main()
+{
+
+}
+
+console.log(777)
+appendFileSync("log636echo.log", "777")
 
 // Electron会在初始化完成并且准备好创建浏览器窗口时调用这个方法
 // 部分 API 在 ready 事件触发后才能使用。
 app.whenReady().then(createWindow)
 
+console.log(888)
+appendFileSync("log636echo.log", "888")
 //当所有窗口都被关闭后退出
 app.on('window-all-closed', () => {
     // 在 macOS 上，除非用户用 Cmd + Q 确定地退出，
@@ -70,3 +40,108 @@ app.on('window-all-closed', () => {
         app.quit()
     }
 })
+
+app.on("keydown", () => {
+    //looks not hav this evt
+    console.log(arguments)
+    appendFileSync("log636echo.log", "keydown")
+})
+app.on('ready', () => {
+    // get the mouse position
+    var electron = require('electron');
+    var mousePos = electron.screen.getCursorScreenPoint();
+
+
+    //let mousePos = screen.getCursorScreenPoint();
+    // var {remote}=require('electron');
+
+    electron.dialog.showErrorBox('title', JSON.stringify(mousePos));
+    console.log(mousePos);
+    setWinPosNsize()
+});
+appendFileSync("log636echo.log", "999")
+console.log(999)
+
+
+/**
+ *  last la
+ */
+function createWindow() {
+    // 创建浏览器窗口
+    const win = new BrowserWindow({
+        icon: "res/icon.jpg",
+        width: 1024,
+        height: 230,
+        x: 20,
+        y: 1,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+            enableRemoteModule: true
+        }
+    })
+    global['win'] = win
+    win.setVisibleOnAllWorkspaces(true, {visibleOnFullScreen: true});
+    win.setAlwaysOnTop(true, "floating");
+    // Below statement completes the flow
+    win.moveTop();
+
+    // 并且为你的应用加载index.html
+    win.loadFile('echo.html')
+
+    var ini = require("ini")
+    var path = require("path")
+    var fs = require("fs")
+    const iopath = path.join(__dirname, './cfg.ini'); // 引用Pos.ini的相对地址
+    const Info = ini.parse(fs.readFileSync(iopath, 'utf-8'));
+
+    require("./libx/logger")
+    log_info("Info.debug " + Info.debug)
+    console.log("Info.debug " + Info.debug)
+
+
+    // const fs = require('fs');
+//
+// directory to check if exists
+    const dir = 'c:/debug';
+
+// check if directory exists
+//
+
+    //dbg
+    //   win.openDevTools();// win.openDevTools();
+
+
+}
+
+
+// la  l l  aaaa la kkk law  law  la
+// set sieze la  law l law law l st
+// tbr
+/**
+ * set win wz mouse pos  ,,n ech win size
+ */
+function  setWinPosNsize()
+{
+    setInterval(() => {
+        try {
+            var electron = require('electron');
+            var mousePos = electron.screen.getCursorScreenPoint();
+
+            global['win'].setPosition(100, mousePos.y + 60)
+
+            var {readFileSync, writeFileSync, appendFileSync} = require("fs");
+          let   txt = readFileSync("setSize.txt").toString()
+            let arr = txt.split(",")
+            let wid = parseInt(arr[0])
+            let ht = parseInt(arr[1])
+            global['win'].setSize(wid, ht)
+        } catch (e) {
+            console.log(e)
+           // throw e
+        }
+
+    }, 200)
+}
+
+// l  l l ls
--- libx/kbdLsn.js
+++ libx/kbdLsn.js
@@ -1,76 +1,121 @@
-f = "C:\\w\\iptmthd\\curKey.txt"
-
-
+//f = "C:\\w\\iptmthd\\keysdir\\curKey.txt"
+var buff = ""
 var {readFileSync, writeFileSync, appendFileSync} = require("fs");
+const fs = require('fs')
+
+
+/**
+ * la law 法律
+ */
+function  main()
+{
+    const prcsmd = require("child_process");
+    const filePath = 'C:\\w\\iptmthd\\keysdir\\'
+    console.log(`正在监听 ${filePath}`);
+    fs.watch(filePath, (event, filename) => {
+        console.log("event=>" + event)
+        console.log("filename=>" + filename)
+        try {
+            if (event == "change") {
+                if (global[event + filename])
+                    return
+                global[event + filename] = true;// read
+                console.log(`${filename}文件发生更新`)
+                let curiptWd = readFileSync(filePath + "/" + filename).toString()
+                console.log(curiptWd)
+                curiptWdLow = curiptWd.toLowerCase()
+                if (curiptWdLow == "{space}" || curiptWdLow == "{enter}") {
+                    console.log("out buff=>" + buff)
+                    findWdByBufpartNwrtToUi(buff)
+                    buff = ""
+                    setEchoWinTxt(buff);
+                }
+                if (curiptWdLow == "{backspace}") {
+                    console.log("  buff=>" + buff)
+                    let lastIdx = buff.length - 1
+                    buff = buff.substring(0, lastIdx)
+
+                    console.log("  buff=>" + buff)
+                    findWdLstByPartwd(buff)
+                }
+                if (curiptWd.startsWith("{")) {
+                    return;
+                } else {
+                    buff = buff + curiptWd;
+                    console.log("buff=>" + buff)
+                    findWdLstByPartwd(buff)
+                }
+            }
+        } catch (e) {
+            console.log(e)
+        }
+
+    })
+
+}
+
 // txt = readFileSync(iomapfile)
 // jsonarr = JSON.parse(txt)
+// last 最后的 la
 
-var buff = ""
-const fs = require('fs')
-const filePath = 'C:\\w\\iptmthd\\'
-console.log(`正在监听 ${filePath}`);
+main()
 
+/**
+ *
+ * @param echoWinTxt
+ */
 function setEchoWinTxt(echoWinTxt) {
     writeFileSync("iptecho.txt", echoWinTxt)
 }
 
-fs.watch(filePath, (event, filename) => {
-    console.log("event=>" + event)
 
-    console.log("filename=>" + filename)
+/** csl cs
+ * undefined undefi nedundefined undefined
+ * undefined
+ * undefined  law 法律 jst 就 我 nam 名字
+ *  law  law law last  last
+ * law LAW LAW LAW LAW LAW law
+ * @param buff
+ * @constructor
+ */
+function findWdByBufpartNwrtToUi(buff) {
+
+    console.log("[iptBufToUI]"+ JSON.stringify(arguments) )
+   let cnstr=findWdByBufpart(buff)
+    if(!cnstr)
+        return
+    console.log("[Buf2wdWrtToUI]cnstr=>"+cnstr)
+
+
     try {
-        if (event == "change") {
-            if (global[event + filename])
-                return
-            global[event + filename] = true;// read
+        let exe = "C:\\prgrm\\AutoIt3\\AutoIt3.exe"
+        let cmd = exe + "  writeToUi.au3 " + cnstr;
 
-            console.log(`${filename}文件发生更新`)
+        console.log(cmd)
+       // return
+        var prcsmd = require('child_process');
+        // let rzt =
+        prcsmd.execSync(cmd, {
 
-            let curiptWd = readFileSync(filePath + "/" + filename).toString()
-
-            console.log(curiptWd)
-
-            curiptWdLow = curiptWd.toLowerCase()
+            maxBuffer: 20000 * 1024,
 
 
-            if (curiptWdLow == "{space}" || curiptWdLow == "{enter}") {
-                console.log("out buff=>" + buff)
-                buff = ""
-                let echoWinTxt=""
-                setEchoWinTxt(echoWinTxt);
-            }
+        }).toString();
 
-            if (curiptWdLow == "{backspace}"   ) {
-                console.log("  buff=>" + buff)
-                let lastIdx=buff.length-1
-                buff =buff.substring(0,lastIdx)
-
-                console.log("  buff=>" + buff)
-                show_buf2wdlst(buff)
-            }
-
-            if (curiptWd.startsWith("{")) {
-                return;
-            } else {
-                buff = buff + curiptWd;
-                console.log("buff=>" + buff)
-                show_buf2wdlst(buff)
-            }
-
-
-        }
     } catch (e) {
-        // lk l  lk lk like
-
         console.log(e)
+
     }
 
-})
+}
 
 
-
-function show_buf2wdlst(curiptWd) {
-
+/**
+ *  csl 控制台 csl 控制台
+ * @param curiptWd
+ * @returns {*}
+ */
+function findWdByBufpart(curiptWd) {
 
 
     iomapfile = "C:\\modyfing\\apiprj\\jbbot\\zmng\\libx\\ipt_iomap.json"
@@ -80,21 +125,50 @@ function show_buf2wdlst(curiptWd) {
     jsonarr = JSON.parse(txt)
 
 
-  //  curiptWd = readFileSync(cuript).toString()
+    //  curiptWd = readFileSync(cuript).toString()
     curiptWd = curiptWd.toLowerCase()
 
     if (curiptWd.startsWith("{"))
         return
     showecho = ""
     for (jsonobj of jsonarr) {
-        if (jsonobj.key.startsWith(curiptWd)) {
+        if (jsonobj.key==(curiptWd)) {
+            return jsonobj.cnstr
+        }
+
+    }
+
+}
+
+/**
+ *
+ * @param curWdpart
+ */
+function findWdLstByPartwd(curWdpart) {
+
+
+    iomapfile = "C:\\modyfing\\apiprj\\jbbot\\zmng\\libx\\ipt_iomap.json"
+
+    var {readFileSync, writeFileSync, appendFileSync} = require("fs");
+    txt = readFileSync(iomapfile)
+    jsonarr = JSON.parse(txt)
+
+
+    //  curiptWd = readFileSync(cuript).toString()
+    curWdpart = curWdpart.toLowerCase()
+
+    if (curWdpart.startsWith("{"))
+        return
+    showecho = ""
+    for (jsonobj of jsonarr) {
+        if (jsonobj.key.startsWith(curWdpart)) {
             showecho = showecho + " " + jsonobj.key + "" + "" + jsonobj.cnstr + " "
         }
 
     }
     console.log(showecho)
     setEchoWinTxt(showecho);
-   // writeFileSync("iptecho.txt",showecho)
+    // writeFileSync("iptecho.txt",showecho)
 }
 
 // setInterval(()=>{
